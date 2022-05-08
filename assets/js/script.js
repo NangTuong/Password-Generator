bcc70a354f1db61634472b2754b7637872739738
NangTuong <nguyen.brad@outlook.com>
2022-05-08 11:15:07 -0400

    created variables for acceptable password characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
-// Assignment code here
+// array of acceptable characters
+var lowerCaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var upperCaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
+var specialCharacters = ['@', '%', '+', '/', '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.']
 
+console.log(specialCharacters);
+
+// function for user password criteria
+var passwordCriteria = () => {
+  var passwordLength = window.prompt("How many characters would you like your password to have? Must be a value between 8 and 128.");
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
